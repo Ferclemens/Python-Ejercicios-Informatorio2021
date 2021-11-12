9b0c6552aae682534520e216b1ead0423b4cb617
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2021-11-12 15:17:46 -0300

    mejorando AGENDA (poo4)

--- poo4.py
+++ poo4.py
@@ -4,6 +4,26 @@ class Agenda():
     def __init__(self):
         self.lista_contactos = []
     
+    def menu(self):
+        print("------------MENU AGENDA------------")
+        print("Seleccione con 1-2-3-4-5 la acción requerida:\n")
+        print("1 - Agregar Contacto.\n2 - Buscar Contacto.\n3 - Editar Contacto.\n4 - Eliminar Contacto.\n5 - Cerrar Agenda.")
+        eleccion = int(input("Su elección: "))
+        if eleccion == 1:
+            self.agregarContacto()
+        elif eleccion == 2:
+            id = int(input("seleccione ID de contacto a editar - Enter para buscar por otro parametro."))
+            nombre = input("seleccione NOMBRE de contacto a editar - Enter para buscar por otro parametro.")
+            telefono = int(input("seleccione TELEFONO de contacto a editar - Enter para buscar por otro parametro."))
+            mail = input("seleccione MAIL de contacto a editar - Enter para buscar por otro parametro.")
+            self.buscarContacto(self,id,nombre,telefono,mail)
+        elif eleccion == 3:
+            self.editarContacto()
+        elif eleccion == 4:
+            self.eliminarContacto()
+        else:
+            pass
+
     def agregarContacto(self):
         salir = 1
         while True and salir != 0:
@@ -14,38 +34,35 @@ class Agenda():
             mail = input("ingrese mail: ")
             contacto = Contacto(id,nombre,telefono,mail)
             self.lista_contactos.append(contacto)
-            salir = int(input("continuar cargando contactos? Si - 1 / NO - 0\nSu elección: "))
-        #print(self.__str__())
+            salir = int(input("continuar cargando contactos? 1 - SI / 0 - NO\nSu elección: "))
+        self.menu()
         
     def buscarContacto(self,id=None,nombre=None,telefono=None,mail=None):
         for a in self.lista_contactos:
             if a.id == id or a.nombre == nombre or a.telefono == telefono or a.mail == mail:
                 return f"RESULTADO BUSCADOR:\n{a.__str__()}"
 
-    def editarContacto(self,id):
-        print(f"ID CONTACTO A EDITAR:\n{id}")
-        #index = self.lista_contactos.id
-        nuevoContacto = Agenda.agregarContacto
-        contacto = nuevoContacto
-        #self.lista_contactos[index] = contacto
-        return f"CONTACTO EDITADO:\n{contacto}"
+    def editarContacto(self,id,nvonombre,nvotelefono,nvomail):
+        print(f"CONTACTO A EDITAR:\n{self.lista_contactos[id-1].__str__()}")
+        nuevoContacto = Contacto(id,nvonombre,nvotelefono,nvomail)
+        self.lista_contactos[id-1] = nuevoContacto
+        return f"CONTACTO EDITADO:\n{nuevoContacto.__str__()}"
     
     def eliminarContacto(self, id):
-        print(f"CONTACTO ELIMINADO")
-        print(f"{id}")
-        index = self.lista_contactos.index(id)
-        self.lista_contactos.pop(index)
-        return f"{self.__str__()}"
+        print(f"CONTACTO A ELIMINAR")
+        print(f"{self.lista_contactos[id].__str__()}")
+        self.lista_contactos.pop(id)
+        print(f"CONTACTO ELIMINADO CON EXITO")
+        return f"{self.lista_contactos.__str__()}"
 
     def __str__(self):
-        print("AGENDA COMPLETA:\n")
-        agenda = []
-        for contacto in self.lista_contactos:
-            agenda.append(print(f"{contacto.__str__()}"))
-        return f"{agenda}"
+        print("AGENDA COMPLETA:")
+        for a in self.lista_contactos:
+            print(a)
 
     def salir():
         pass
+        
 
 class Contacto():
     def __init__(self,id,nombre,telefono,mail):
@@ -55,16 +72,22 @@ class Contacto():
         self.mail = mail
 
     def __str__(self):
-        return f"CONTACTO:\nID: {self.id}\nNombre: {self.nombre}\nTeléfono: {self.telefono}\nMail: {self.mail}\n---"
+        return f"ID: {self.id}\nNombre: {self.nombre}\nTeléfono: {self.telefono}\nMail: {self.mail}\n----------"
 
-print("----------------------AGENDA PERSONAL--------------------")
+print("------------AGENDA PERSONAL------------")
 
 agenda1 = Agenda()
-agenda1.agregarContacto()
-print(agenda1)
+agenda1.menu()
+"""agenda1.agregarContacto()
+agenda1.__str__()
+agenda1.buscarContacto(telefono=654)
+agenda1.editarContacto(1,nvonombre="Roman",nvotelefono=543,nvomail="fdasfds")
+agenda1.eliminarContacto(2)
+agenda1.__str__()"""
 
-print(agenda1.buscarContacto("fer"))
-print(agenda1.editarContacto(1))
+#print(agenda1.buscarContacto(nombre="fer"))
+#print(agenda1.editarContacto(1,nvonombre="ramon",nvotelefono=555,nvomail="ytr"))
+#print(agenda1)
 
 """salir = 1
 while True and salir != 0:
